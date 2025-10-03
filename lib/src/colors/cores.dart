bc8dcf645763e9c03ec5aa22ee4542e8d14c7f45
Vinicius <vini.Stormtrooper@gmail.com>
2025-10-03 14:46:30 -0300

    alteracoes no appBar

--- lib/src/colors/cores.dart
+++ lib/src/colors/cores.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Cores {
-  Color mainColor = Color(0xff47ED7E);
+  Color mainColor = Color(0xff4ab7b6);
   Color foregroundColor = Colors.white;
   Color textColor = Color(0xff000000);
 }
--- lib/src/widgets/custom_app_bar.dart
+++ lib/src/widgets/custom_app_bar.dart
@@ -1,10 +1,10 @@
 import 'package:ecommerce_app/src/colors/cores.dart';
 import 'package:flutter/material.dart';
 
-class CustomAppBar extends StatelessWidget implements PreferredSizeWidget{
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
   final bool? showCarrinho;
-  const CustomAppBar({super.key, required this.title,this.showCarrinho});
+  const CustomAppBar({super.key, required this.title, this.showCarrinho});
 
   @override
   Size get preferredSize => Size.fromHeight(kToolbarHeight);
@@ -12,16 +12,26 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget{
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      leadingWidth: 80,
+      leading: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+        child: Image.asset(
+          'assets/images/logo.png',
+          width: 40, // aumenta largura
+          height: 40, // aumenta altura
+          fit: BoxFit.contain,
+        ),
+      ),
       elevation: 3.5,
-      title: Text(title, style: TextStyle(fontWeight: FontWeight.bold),),
-      backgroundColor: Cores().mainColor,
-      foregroundColor: Cores().foregroundColor,
+      title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+      backgroundColor: Cores().foregroundColor,
+      foregroundColor: Cores().mainColor,
       shadowColor: const Color.fromARGB(200, 8, 8, 8),
       centerTitle: true,
       actions: [
-        if(showCarrinho == true)...[
-          IconButton(onPressed: (){}, icon: Icon(Icons.shopping_cart))
-        ]
+        if (showCarrinho == true) ...[
+          IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
+        ],
       ],
     );
   }
