ed7571b95a4cc1dabeec2a5bfe06b7517393a0e2
Vinicius <vini.Stormtrooper@gmail.com>
2025-10-03 19:13:29 -0300

    criacao das models

---
+++ lib/src/models/produto_model.dart
@@ -0,0 +1,11 @@
+/* class ProdutoModel {
+  final int? id;
+  final String nome;
+  final double preco;
+  final String descricao;
+  final int estoque;
+  final String url_foto;
+  final int id_categoria;
+
+
+} */
