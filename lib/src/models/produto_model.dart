750fa0cd09aff119037f49962c274f72be002e39
Vinicius <vini.Stormtrooper@gmail.com>
2025-10-05 19:01:04 -0300

    feito a model de produto

--- lib/src/models/produto_model.dart
+++ lib/src/models/produto_model.dart
@@ -1,11 +1,40 @@
-/* class ProdutoModel {
+class ProdutoModel {
   final int? id;
   final String nome;
   final double preco;
   final String descricao;
   final int estoque;
-  final String url_foto;
-  final int id_categoria;
+  final String urlFoto;
+  final int idCategoria;
 
+  ProdutoModel({
+    this.id, 
+    required this.nome, 
+    required this.preco, 
+    required this.descricao, 
+    required this.estoque, 
+    required this.urlFoto, 
+    required this.idCategoria,});
 
-} */
+    factory ProdutoModel.fromJson(Map<String, dynamic> json) {
+      return ProdutoModel(
+        nome: json['nome'], 
+        preco: json['preco'], 
+        descricao: json['descricao'], 
+        estoque: json['estoque'], 
+        urlFoto: json['url_foto'], 
+        idCategoria: json['categoria']?['id']);
+    }
+
+    Map<String,dynamic> toJson(){
+      return {
+        'id' :id,
+        'nome': nome,
+        'preco': preco,
+        'descricao': descricao,
+        'estoque': estoque,
+        'url_foto': urlFoto,
+        'categoria': idCategoria
+      };
+    }
+} 
